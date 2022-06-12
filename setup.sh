ad361e4ef0d5cc5c9d0d1dc9f2ce5023040cf29f
Mr-Death-Force-specialChat <84960714+Mr-Death-Force-specialChat@users.noreply.github.com>
2022-06-12 10:20:15 +0400

    Created setup.sh

---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+echo "     _             _                    _               "
+echo "    / \   _ __ ___| |__        ___  ___| |_ _   _ _ __  "
+echo "   / _ \ | '__/ __| '_ \ _____/ __|/ _ \ __| | | | '_ \ "
+echo "  / ___ \| | | (__| | | |_____\__ \  __/ |_| |_| | |_) |"
+echo " /_/   \_\_|  \___|_| |_|     |___/\___|\__|\__,_| .__/ "
+echo "                                                 |_|    "
+echo "                            _             __  __     _  __      "
+echo "                           | |__  _   _  |  \/  | __| |/ _|_  __"
+echo "                           | '_ \| | | | | |\/| |/ _\` | |_\ \/ /"
+echo "                           | |_) | |_| | | |  | | (_| |  _|>  < "
+echo "                           |_.__/ \__, | |_|  |_|\__,_|_| /_/\_\\"
+echo "                                  |___/                         "
+
+echo "author:           Mdfx"
+echo "contributors:     [Mdfx]"
+echo "source:           https://github.com/Mr-Death-Force-specialChat/arch-setup/"
+echo "description: \"A tool to quickly setup archlinux\""
+
+echo "1. Standard (neofetch, cmatrix, vim, sl)"
+echo "2. Devel (base-devel, cmake, code, vim, g++)"
+echo "3. i3 (i3)"
+echo "4. sddm (sddm)"
+
+echo "Select an option: "
+
+read Opt
+
+if [ "$Opt" == "1" ]; then
+    echo "Updating..."
+    sudo pacman -Sy
+    echo "Download Standard packages"
+    sudo pacman -S neofetch cmatrix vim sl
+elif [ "$Opt" == "2" ]; then
+    echo "Updating..."
+    sudo pacman -Sy
+    echo "Download Devel packages"
+    sudo pacman -S base-devel cmake code vim g++
+elif [ "$Opt" == "3" ]; then
+    echo "Updating..."
+    sudo pacman -Sy
+    echo "Download i3(-gaps)"
+    sudo pacman -S i3
+elif [ "$Opt" == "4" ]; then
+    echo "Updating..."
+    sudo pacman -Sy
+    echo "Download sddm"
+    sudo pacman -S sddm
+fi
